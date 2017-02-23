3c912a26254ca57acf91964d875167f3d4795e25
Владислав Титов < >
2017-02-23 21:07:35 +0300

    modal is ended

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@
 }());
 
 $().ready(function () {
-			$("ul.list > li").click(function () {
-				if( $(this).find("input[type='checkbox']").attr("checked") ){
-					$(this).find("input[type='checkbox']").removeAttr("checked");
-				}else{
-					$(this).find("input[type='checkbox']").attr("checked", true);
-				}
-			})
-		});
+	$("ul.list > li").click(function () {
+		if( $(this).find("input[type='checkbox']").attr("checked") ){
+			$(this).find("input[type='checkbox']").removeAttr("checked");
+		}else{
+			$(this).find("input[type='checkbox']").attr("checked", true);
+		}
+	})
+});
