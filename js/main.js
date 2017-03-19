166386908deeef936254dd10d093dcf4234db8c6
VladislavTitov <titovvlad1997@yandex.ru>
2017-03-19 20:59:01 +0300

    update

--- js/main.js
+++ js/main.js
@@ -10,33 +10,6 @@
 	});
 }());
 
-/*(function () {
-	var list = document.querySelectorAll("ul.list > li");
-	for (var i = 0; i < list.length; i++) {	
-		console.log(list[i]);
-		list[i].addEventListener("click", function (argument) {
-			console.log("click on li");
-			var check = list[i].children[0].children[1];
-			if (check.checked === true) {
-				check.checked = false;
-			}else{
-				check.checked = true;
-			}
-		});
-	}
-	
-}());*/
-
-/*$().ready(function () {
-	$("ul.list > li").click(function () {
-		if( $(this).find("input[type='checkbox']").attr("checked") ){
-			$(this).find("input[type='checkbox']").removeAttr("checked");
-		}else{
-			$(this).find("input[type='checkbox']").attr("checked", true);
-		}
-	});
-});*/
-
 function save(key, data) {
 	localStorage.setItem(key.toString(), JSON.stringify(data));
 }
